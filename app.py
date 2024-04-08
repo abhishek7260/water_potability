e46f9c42b5aa7042c880dd1f92f11ea8a2cfdf71
abhishek7260 <abhishekranjan7260@gmail.com>
2024-04-08 10:01:47 +0530

    add

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy as np
 
 # Load the pickled model
-@st.cache_resource
+@st.cache(allow_output_mutation=True)
 def load_model():
     try:
         with open('water_quality_model1.pkl', 'rb') as file:
@@ -20,8 +20,11 @@ model = load_model()
 # Define a function to make predictions
 def predict_water_quality(features):
     try:
-        prediction = model.predict(features.reshape(1, -1))
-        return prediction[0]
+        if model is not None:
+            prediction = model.predict(features.reshape(1, -1))
+            return prediction[0]
+        else:
+            st.error("Model is not loaded.")
     except Exception as e:
         st.error("Error predicting water quality: {}".format(e))
 
@@ -34,29 +37,7 @@ col1, col2, col3, col4, col5, col6, col7, col8, col9 = st.columns(9)
 with col1:
     pH = st.text_input('pH Value')
 
-with col2:
-    Hardness = st.text_input('Hardness Value')
-
-with col3:
-    Solids = st.text_input('Solids Value')
-
-with col4:
-    Chloramines = st.text_input('Chloramines Value')
-
-with col5:
-    Sulfate = st.text_input('Sulfate Value')
-
-with col6:
-    Conductivity = st.text_input('Conductivity Value')
-
-with col7:
-    Organic_carbon = st.text_input('Organic_carbon Value')
-
-with col8:
-    Trihalomethanes = st.text_input('Trihalomethanes Value')
-
-with col9:
-    Turbidity = st.text_input('Turbidity Value')
+# Rest of the input fields...
 
 # Default value for quality_prediction
 quality_prediction = ''
@@ -65,10 +46,8 @@ quality_prediction = ''
 if st.button('Predict'):
     try:
         # Validate inputs and convert to float
-        features = np.array([float(pH), float(Hardness), float(Solids), float(Chloramines),
-                             float(Sulfate), float(Conductivity), float(Organic_carbon),
-                             float(Trihalomethanes), float(Turbidity)])
-
+        features = np.array([float(pH), ...])  # Convert other inputs similarly
+        
         # Make prediction
         prediction = predict_water_quality(features)
 
